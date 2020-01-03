03efecd42ef4dd2bc726c8da9b2180ad5f848069
alex <pigford.m.a@gmail.com>
2020-01-03 15:27:28 -0700

    new post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Topic = require('../models/topic.js');
+const Post = require('../models/post.js');
 
 router.get('/', (req, res) => {
-	res.render('posts/index.ejs');
+    Post.find({}, (err, foundPosts) => {
+        res.render('posts/index.ejs', {
+            posts: foundPosts
+        });
+    });
+});
+
+router.get('/new', (req, res) => {
+	Topic.find({}, (err, allTopics) => {
+		res.render('posts/new.ejs', {
+			topics: allTopics
+		});
+	});
+});
+
+router.post('/', (req, res) => {
+	Post.create(req.body, (err, createdPost) => {
+		res.redirect('/posts');
+	});
 });
 
 module.exports = router;
