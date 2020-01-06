53ac86e0059936ad52a1ce24d32b99969427a982
alex <pigford.m.a@gmail.com>
2020-01-06 12:55:41 -0700

    async/await

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const Topic = require('../models/topic.js');
 const Post = require('../models/post.js');
 
-router.get('/', (req, res) => {
-    Post.find({}, (err, foundPosts) => {
-        res.render('posts/index.ejs', {
-            posts: foundPosts
-        });
-    });
+router.get('/', async (req, res) => {
+    try {
+		const foundPosts = await Post.find();
+
+		res.render('posts/index.ejs', {
+            posts: foundPosts,
+		});
+    } catch (err) {
+		res.send(err);
+    }
 });
 
 router.get('/new', (req, res) => {
@@ -19,41 +23,61 @@ router.get('/new', (req, res) => {
 	});
 });
 
-router.post('/', (req, res) => {
-	Post.create(req.body, (err, createdPost) => {
-		res.redirect('/posts');
-	});
+router.post('/', async (req, res) => {
+    try {
+        await Post.create(req.body);
+
+        res.redirect('/posts');
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-	Post.findById(req.params.id).populate('topic').exec((err, foundPost) => {
-		res.render('posts/show.ejs', {
-			post: foundPost
-		});
-	});
+    try  {
+        const foundPost = await Post.findById(req.params.id);
+
+        res.render('posts/show.ejs', {
+            post: foundPost,
+        });
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-router.get('/:id/edit', (req, res) => {
-	Post.findById(req.params.id, (err, foundPost) => {
-		Topic.find({}, (err, allTopics) => {
-			res.render('posts/edit.ejs', {
-				post: foundPost,
-				topics: allTopics
-			});
-		});
-	});
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const foundPost = await Post.findById(req.params.id);
+
+        const allTopics = await Topic.find();
+
+        res.render('posts/edit.ejs', {
+            post: foundPost,
+            topics: allTopics,
+        });
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
-	Post.findByIdAndUpdate(req.params.id, req.body, () => {
-		res.redirect('/posts');
-	});
+router.put('/:id', async (req, res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, req.body);
+
+        res.redirect(`/posts/${req.params.id}`);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
-	Post.findByIdAndRemove(req.params.id, () => {
-		res.redirect('/posts');
-	});
+router.delete('/:id', async (req, res) => {
+    try {
+        await Post.findByIdAndRemove(req.params.id);
+
+        res.redirect('/posts');
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 module.exports = router;
