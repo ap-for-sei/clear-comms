5ebd3b4984712d22e058175f1a380d063c35a571
alex <pigford.m.a@gmail.com>
2020-01-07 09:39:32 -0700

    heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const session = require('express-session');
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT
 require('./db/db.js');
 
 // middleware
